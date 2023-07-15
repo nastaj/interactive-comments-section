563af935fbdde0d59b9119aab78d6fa78db89f8c
nastaj <G00424689@atu.ie>
2023-07-15 13:29:54 +0200

    current user & adding comments

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,8 @@
 // HTML Elements
 const appContainer = document.querySelector(".container");
+const newCommentForm = document.querySelector(".comment-form");
+const newCommentInput = document.querySelector(".comment-input");
+const newCommentBtn = document.querySelector(".btn-submit");
 
 // Data
 let currentUser = {};
@@ -24,39 +27,40 @@ const renderComments = async function () {
 renderComments();
 
 // Markup
-const generateCommentMarkup = function (comment) {
+const generateCommentMarkup = function (comment, position = "afterbegin") {
   const markup = `
-  <section class="post" author="${comment.user.username}" data-post-id=${
-    comment.id
-  }>
+  ${
+    comment.user === currentUser
+      ? `
+      <section class="post post-own" author="${comment.user.username}" data-post-id=${comment.id}>
     <section class="parent-post">
       <div class="score-container">
-        <button class="btn btn-plus">
-          <img src="img/icon-plus.svg" alt="Plus icon" />
-        </button>
         <p class="score">${comment.score}</p>
-        <button class="btn btn-minus">
-          <img src="img/icon-minus.svg" alt="Minus icon" />
-        </button>
       </div>
 
       <div class="post-main">
-        <header class="post-header">
-          <div class="user-info">
-            <img
-              class="avatar"
-              src="${comment.user.image.webp}"
-              alt="User avatar"
-            />
-            <p class="author">${comment.user.username}</p>
-            <p class="date">${comment.createdAt}</p>
-          </div>
-
-          <button class="btn-reply">
-            <img src="img/icon-reply.svg" alt="Reply icon" />
-            Reply
-          </button>
-        </header>
+      <header class="post-header">
+      <div class="user-info">
+        <img
+          class="avatar"
+          src="${currentUser.image.webp}"
+          alt="User avatar"
+        />
+        <p class="author">${currentUser.username}</p>
+        <p class="author-own">you</p>
+        <p class="date">${comment.createdAt}</p>
+      </div>
+      <div class="btn-container">
+        <button class="btn-delete">
+          <img src="img/icon-delete.svg" alt="Reply icon" />
+          Delete
+        </button>
+        <button class="btn-edit">
+          <img src="img/icon-edit.svg" alt="Reply icon" />
+          Edit
+        </button>
+      </div>
+    </header>
 
         <div class="comment-container">
           <p class="comment">
@@ -65,40 +69,118 @@ const generateCommentMarkup = function (comment) {
         </div>
       </div>
     </section>
+  `
+      : `
+      <section class="post" author="${comment.user.username}" data-post-id=${comment.id}>
+      <section class="parent-post">
+        <div class="score-container">
+          <button class="btn btn-plus">
+            <img src="img/icon-plus.svg" alt="Plus icon" />
+          </button>
+          <p class="score">${comment.score}</p>
+          <button class="btn btn-minus">
+            <img src="img/icon-minus.svg" alt="Minus icon" />
+          </button>
+        </div>
+  
+        <div class="post-main">
+          <header class="post-header">
+            <div class="user-info">
+              <img
+                class="avatar"
+                src="${comment.user.image.webp}"
+                alt="User avatar"
+              />
+              <p class="author">${comment.user.username}</p>
+              <p class="date">${comment.createdAt}</p>
+            </div>
+  
+            <button class="btn-reply">
+              <img src="img/icon-reply.svg" alt="Reply icon" />
+              Reply
+            </button>
+          </header>
+  
+          <div class="comment-container">
+            <p class="comment">
+              ${comment.content}
+            </p>
+          </div>
+        </div>
+      </section>
+  `
+  }
 
     <section class="replies-container">
     ${comment.replies
       .map((reply) => {
         return `
         <section class="post reply-post" data-post-id="${reply.id}">
-          <div class="score-container">
-            <button class="btn btn-plus">
-              <img src="img/icon-plus.svg" alt="Plus icon" />
+        ${
+          reply.user.username === currentUser.username
+            ? `
+        <div class="score-container">
+          <p class="score">${reply.score}</p>
+        </div>
+          `
+            : `
+        <div class="score-container">
+          <button class="btn btn-plus">
+            <img src="img/icon-plus.svg" alt="Plus icon" />
+          </button>
+          <p class="score">${reply.score}</p>
+          <button class="btn btn-minus">
+            <img src="img/icon-minus.svg" alt="Minus icon" />
+          </button>
+        </div> 
+          `
+        }
+          <div class="post-main">
+          ${
+            reply.user.username === currentUser.username
+              ? `
+          <header class="post-header">
+          <div class="user-info">
+            <img
+              class="avatar"
+              src="${currentUser.image.webp}"
+              alt="User avatar"
+            />
+            <p class="author">${currentUser.username}</p>
+            <p class="author-own">you</p>
+            <p class="date">${reply.createdAt}</p>
+          </div>
+          <div class="btn-container">
+            <button class="btn-delete">
+              <img src="img/icon-delete.svg" alt="Reply icon" />
+              Delete
             </button>
-            <p class="score">${reply.score}</p>
-            <button class="btn btn-minus">
-              <img src="img/icon-minus.svg" alt="Minus icon" />
+            <button class="btn-edit">
+              <img src="img/icon-edit.svg" alt="Reply icon" />
+              Edit
             </button>
           </div>
-
-          <div class="post-main">
-            <header class="post-header">
-              <div class="user-info">
-                <img
-                  class="avatar"
-                  src="${reply.user.image.webp}"
-                  alt="User avatar"
-                />
-               <p class="author">${reply.user.username}</p>
-               <p class="date">${reply.createdAt}</p>
-              </div>
-        
-              <button class="btn-reply">
-                <img src="img/icon-reply.svg" alt="Reply icon" />
-                  Reply
-              </button>
-            </header>
-        
+        </header>
+          `
+              : `
+          <header class="post-header">
+          <div class="user-info">
+            <img
+              class="avatar"
+              src="${reply.user.image.webp}"
+              alt="User avatar"
+            />
+           <p class="author">${reply.user.username}</p>
+           <p class="date">${reply.createdAt}</p>
+          </div>
+    
+          <button class="btn-reply">
+            <img src="img/icon-reply.svg" alt="Reply icon" />
+              Reply
+          </button>
+        </header>
+          `
+          }
           <div class="comment-container">
             <p class="comment">
               <span class="replying-to">@${reply.replyingTo}</span>
@@ -113,7 +195,7 @@ const generateCommentMarkup = function (comment) {
     </section>
   `;
 
-  appContainer.insertAdjacentHTML("afterbegin", markup);
+  appContainer.insertAdjacentHTML(position, markup);
 };
 
 // Scores
@@ -123,6 +205,7 @@ const setScore = async function () {
   const scoreContainer = document.querySelectorAll(".score-container");
   scoreContainer.forEach((score) =>
     score.addEventListener("click", (e) => {
+      console.log("Click");
       // Refactor this mess later
       const currentComment = comments.find(
         (comment) =>
@@ -161,3 +244,21 @@ const setScore = async function () {
   );
 };
 setScore();
+
+newCommentForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const newComment = {
+    id: Math.trunc(Math.random() * 100),
+    content: newCommentInput.value,
+    createdAt: "Now",
+    replies: [],
+    score: 0,
+    user: currentUser,
+  };
+
+  newCommentInput.value = "";
+  comments.push(newComment);
+
+  generateCommentMarkup(newComment);
+});
